3f592291b7bc587455a7d20f3154c23633551dea
Wrenchx <wzs84ahh@gmail.com>
2023-12-24 04:42:39 +0100

    Update login page and table styling

--- routes/signIn.js
+++ routes/signIn.js
@@ -4,7 +4,7 @@ const { event } = require("../models")
 
 /* GET users listing. */
 router.get('/', async function(req, res) {
-
+	res.render("signIn", { title: 'Login| Kikis Delivery Service' });
 });
 
 module.exports = router;
